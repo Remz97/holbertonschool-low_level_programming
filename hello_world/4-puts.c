d3479379624e3641143ed365c2d05fb8abea87ed
Remz97 <remzikeqi@icloud.com>
2023-10-13 00:57:20 -0700

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -4,7 +4,7 @@
  * Description: 'main function description'
  * Return: Always 0 (Success)
  */
-int void main(void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
