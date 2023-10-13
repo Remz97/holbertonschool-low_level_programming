dbd57bf028676789b7784cfb9fdb09e632e91dbc
Remz97 <remzikeqi@icloud.com>
2023-10-13 11:15:17 -0700

    101-quote.c

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  *  *  * main - Entry point
  *  *  *
@@ -6,6 +7,7 @@
  */
 int main(void)
 {
-	sprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	ssize_t write(2, and that piece of art is useful" - Dora Korpar, 2015-10-19\n);
 	return (1);
 }
