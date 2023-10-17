e41a193581af6af3d333827ba91a8654c3cc8b90
Remz97 <remzikeqi@icloud.com>
2023-10-17 03:37:29 -0700

    3-print_alphabets.c

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: void
+ */
+
+int main(void)
+{
+	char i;
+	char j;
+
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+	for (j = 'A'; j <= 'Z'; j++)
+		putchar(j);
+	putchar('\n');
+	return (0);
+}
