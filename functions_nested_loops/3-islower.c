0e655eeb98867e2acfb8c1e3af257caa50cd73ce
Remz97 <remzikeqi@icloud.com>
2023-10-18 04:52:16 -0700

    3-islower

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * int _islower - entry point
+ *
+ * Return: void
+ */
+int _islower(int c)
+
+{
+	
+	if (c > 96 && c < 123)
+	{
+		return (1);
+	} else {
+		return (0);
+	}
+
+}
