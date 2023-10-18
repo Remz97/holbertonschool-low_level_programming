02d6cd5629d14258d6cd31f37a65b643b7ae1694
Remz97 <remzikeqi@icloud.com>
2023-10-18 07:17:52 -0700

    6-abs.c

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _abs - Entry point
+ * main.h header file
+ * @n: is parameter
+ * Return:0
+ */
+
+int _abs(int n)
+
+{
+	if (n < 0)
+	{
+		_putchar (n * (-1));
+	}
+	return (0);
+}
