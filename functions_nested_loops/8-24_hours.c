17225f566e4f20c01725c4f705067d6f4d185f2b
Remz97 <remzikeqi@icloud.com>
2023-10-19 02:42:01 -0700

    8-main.c

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * jack_bauer - Entry point
+ * @void is parameter
+ * return abs(void)
+ * Return:0
+ */
+void jack_bauer(void)
+
+{
+	int min;
+	int hour;
+
+	for (hour = 0; hour < 24; hour++)
+	{
+	for (min = 0; min < 60; min++)
+	{
+		_putchar('0' + (hour / 10));
+		_putchar('0' + (hour % 10));
+		_putchar(';');
+		_putchar('0' + (min / 10));
+		_putchar('0' + (min % 10));
+		_putchar('\n');
+	}
+	}
+}
