592e65470ab7c0200bcb4735338cef11caea56db
Remz97 <remzikeqi@icloud.com>
2023-10-19 08:12:21 -0700

    7-print_last_digit.c

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * add - Entry point
+ * @a: int to be added to b
+ * @b: int to be added to a
+ * Return:0
+ */
+void print_to_98(int n)
+{
+	for (n = 0; n < 99; n++)
+	{
+		printf("%d, \n", n);
+	}
+}
