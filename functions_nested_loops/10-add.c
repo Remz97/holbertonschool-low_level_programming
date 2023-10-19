5e9a52231a1fd498bc1ef6e72711d70b158dbac5
Remz97 <remzikeqi@icloud.com>
2023-10-19 05:48:18 -0700

    10-add.c

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - Entry point
+ * @a: int to be added to b
+ * @b: int to be added to a
+ * Return:0
+ */
+int add(int a, int b)
+{
+	int add;
+
+	add = a + b;
+	return (add);
+}
