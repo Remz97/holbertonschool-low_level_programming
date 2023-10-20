dbddf164fbaaaa6081de134c3e71b05c19260cc0
Remz97 <remzikeqi@icloud.com>
2023-10-20 03:24:04 -0700

    2-mul.c

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - Entry point
+ * @a: is int
+ * @b: is int
+ * Print natural number until 98;
+ * Return: 0
+ */
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+	return (mul);
+}
