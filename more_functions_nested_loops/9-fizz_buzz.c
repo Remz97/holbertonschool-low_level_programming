2d7a379efdba1ebc4ab3476b1df9b702539702a2
Remz97 <remzikeqi@icloud.com>
2023-10-21 16:48:51 -0700

    10-print_triangle.c

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int n;
+	for (n = 0; n <= 100; n++)
+	{
+		if (n * 3)
+			printf("Fizz");
+		else if (n * 5)
+			printf("Buzz");
+		else
+	}
+		printf("\n");
+		return (0);
+}
