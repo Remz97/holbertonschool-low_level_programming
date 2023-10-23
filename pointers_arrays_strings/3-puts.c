95fbcfc75226f6d55e2f191a5c3bea8eae943260
Remz97 <remzikeqi@icloud.com>
2023-10-23 04:48:20 -0700

    4-print_rev.c

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - main
+ * @n: is int
+ *
+ */
+void _puts(char *str)
+{
+
+}
---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_rev - main
+ * @s: is int
+ *
+ */
+void print_rev(char *s)
+{
+	int i;
+	int length = 0;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+	for (i = length - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+
+}
