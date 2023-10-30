7401fa1c0a07e43926cacc3a5d1938af355cb4d1
Remz97 <remzikeqi@icloud.com>
2023-10-30 06:23:03 -0700

    2-strlen_recursion.c

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
- *_strlen_recursion - asasd
+ *_strlen_recursion - l
  *@s: aedx
+ *Return: length
  */
 int _strlen_recursion(char *s)
 {
 	if (*s)
 		return (1 + _strlen_recursion(s + 1));
 	else
-		return (0); 
+		return (0);
 }
