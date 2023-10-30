c2bb80dc9712d580c43cbcc9d96a8fb5c251dd54
Remz97 <remzikeqi@icloud.com>
2023-10-30 08:21:14 -0700

    3-factorial.c

---
+++ recursion/3-factorial.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *factorial - sdfc
+ *@n: adscas
+ *Return: ased
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0 || n == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
