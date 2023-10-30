ac8d3b1446243e230e68fc1920611816c83c696f
Remz97 <remzikeqi@icloud.com>
2023-10-30 08:51:04 -0700

    4-pow_recursion.c

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *_strlen_recursion - sdfc
+ *@x: adscas
+ *@y: fd
+ *Return: ased
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+}
