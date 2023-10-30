1dd15cea19d6e38bdda60e221aae10f719cc5d86
Remz97 <remzikeqi@icloud.com>
2023-10-30 06:02:22 -0700

    1-print_rev_recursion.c

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *_puts_recursion - asasd
+ *@s: aedx
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
