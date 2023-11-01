cd7604d292253e5c1f8a352e599e84fa78ab7a78
Remz97 <remzikeqi@icloud.com>
2023-11-01 12:04:30 -0700

    2-args.c

---
+++ argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - main funcion
+ * @argc: arguments past in unix count
+ * @argv: strings pasted
+ * ne vend te int argc mund te __attribute__((unused)) argc
+ * Return: Return 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n ", argv[i]);
+	}
+		return (0);
+}
