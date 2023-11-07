c6506dd20a8950e6169e0d457e42e6e51e329d98
Remz97 <remzikeqi@icloud.com>
2023-11-07 02:07:45 -0800

    2-str_concat.c

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,31 +1,28 @@
-#include "main.h"
 #include <stdlib.h>
+#include <string.h>
+/**
+ * str_concat - function to concate 2 arry
+ * @s1: first string
+ * @s2: second string
+ * Return: null or a pointer to s
+ */
 char *str_concat(char *s1, char *s2)
 {
-	int i;
-	int str_length_1 = 0;
-	int str_length_2 = 0;
-	int total_length;
-	char *concatenated;
+	unsigned int i;
+	char *s;
 
 	if (s1 == NULL)
-		return NULL;
+		s1 = "";
 	if (s2 == NULL)
-		return NULL;
-    // 1. find length of s1 and s2
-	while (s1[str_length_1] != '\0')
-		str_length_1++;
-	while (s2[str_length_2] != '\0')
-		str_length_2++;
-    // 2. malloc for concatenated
-	total_length = str_length_1 + str_length_2;
-	concatenated = malloc(sizeof(char) * (total_length + 1));
-	if (concatenated == NULL)
-		return NULL;
-    // 3. copy s1 to concatenated; then copy s2 to concatenated
-	for (i=0; s1[i] != '\0'; i++)
-		concatenated[i] = s1[i];
-	for (i=0; s2[i] != '\0'; i++)
-		concatenated[str_length_1 + i] = s2[i];
-		return concatenated;
+		s2 = "";
+
+	s = malloc(sizeof(char) * (strlen(s1) + strlen(s2) + 1));
+	if (s == NULL)
+		return (NULL);
+	for (i = 0; i < strlen(s1); i++)
+		s[i] = s1[i];
+	for (i = 0; i < strlen(s2); i++)
+		s[strlen(s1) + i] = s2[i];
+	s[strlen(s1) + i] = '\0';
+	return (s);
 }
