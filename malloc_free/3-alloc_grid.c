2fd1b2fbda60e156de7d4d0cdbe3597630d52d23
Remz97 <remzikeqi@icloud.com>
2023-11-07 02:03:50 -0800

    3-alloc_grid.c

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -1,42 +1,42 @@
-#include "main.h"
 #include <stdlib.h>
+#include <string.h>
 /**
  * alloc_grid - function to allocate matrix
  * @width: columns
  * @height: height
  * Return: null or a pointer to matrix
  */
-
 int **alloc_grid(int width, int height)
 {
 	int **my_array;
-	int j, k;
+	int i, j;
 
-	if (width <= '0' || height <= 0)
+	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	my_array = malloc(sizeof(int *) * width);
+	my_array = malloc(sizeof(int *) * height);
 	if (my_array == NULL)
 		return (NULL);
-	for (j = 0; j < height; j++)
+
+	for (i = 0; i < height; i++)
 	{
-		my_array[j] = malloc(sizeof(int) * width);
-		if (my_array[j] == NULL)
+		my_array[i] = malloc(sizeof(int) * width);
+		if (my_array[i] == NULL)
 		{
-			while (j >= 0)
+			while (i >= 0)
 			{
-				free(my_array[j]);
-				j--;
+				free(my_array[i]);
+				i--;
 			}
 			free(my_array);
 			return (NULL);
 		}
 	}
-	for (j = 0; j < width; j++)
+	for (i = 0; i < height; i++)
 	{
-		for (k = 0; k < height; k++)
+		for (j = 0; j < width; j++)
 		{
-			my_array[j][k] = 0;
+			my_array[i][j] = 0;
 		}
 	}
 	return (my_array);
