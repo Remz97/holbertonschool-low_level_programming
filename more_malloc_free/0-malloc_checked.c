f73826e4c86114b6fc5a18339be6d09ccd885278
Remz97 <remzikeqi@icloud.com>
2023-11-07 04:45:44 -0800

    0-malloc_checked.c

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,6 +1,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/**
+ * *malloc_checked - ss
+ * @b: sf
+ * 
+ **/
+
 void *malloc_checked(unsigned int b)
 {
 	void *ptr = malloc(b);
@@ -9,6 +15,6 @@ void *malloc_checked(unsigned int b)
 	{
 		exit(98);
 	}
-	return ptr;
+	return (ptr);
 }
 
