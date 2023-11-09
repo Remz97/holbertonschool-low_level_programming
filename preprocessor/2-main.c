6fee2a5ad80e409d848ce41901f9ef3d1e75f8d1
Remz97 <remzikeqi@icloud.com>
2023-11-09 02:10:16 -0800

    2-main.c

--- preprocessor/2-main.c
+++ preprocessor/2-main.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
 int main (void)
 {
-	printf __FILE__ ("2-main.c\n");
+	printf ("%s\n", __FILE__);
 	return (0);
 }
